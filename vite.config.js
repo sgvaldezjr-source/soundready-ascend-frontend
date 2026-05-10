8447f9a64119b691ba39283ee44431100b11d3b5
sgvaldezjr-source <sgvaldezjr@gmail.com>
2026-05-11 07:11:46 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
- 
+
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+    port: 3000,
   },
   preview: {
-    host: true,
-    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    host: "0.0.0.0",
+    port: parseInt(process.env.PORT) || 4173,
     allowedHosts: true,
   },
 });
